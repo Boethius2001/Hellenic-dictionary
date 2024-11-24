9d775306072c2eb1649aab543920de07fc4eb0e4
Boethius <emrecan.senel2001@gmail.com>
2024-11-24 13:30:19 +0300

    adjusted style

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const express = require('express');
-const app = express();
+const path = require('path');
 
+const app = express();
 const PORT = 3000;
+
+// deneme
+app.use(express.static(path.join(__dirname, 'views')));
+app.use('/src',express.static(path.join(__dirname,'src')));
+app.use('/styles',express.static(path.join(__dirname,'styles')));
+
+app.get('/',(req, res)=>{
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
 
 app.listen(PORT, ()=>{
     console.log("Listening on 3000");
